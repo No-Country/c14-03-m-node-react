8bae9596ba28efe159514f119f4629962d3ad94c
Gonzalo Faust <gonzalofaust@gmail.com>
2023-10-17 10:37:21 -0300

    add trailer component and small changes

--- cliente/src/components/atoms/starRating/index.jsx
+++ cliente/src/components/atoms/starRating/index.jsx
@@ -2,44 +2,13 @@ import React from 'react'
 import { BsStarFill } from 'react-icons/bs'
 
 function StarRating ({ item }) {
-    const rateScale = [
-        {
-            name: 'bad',
-            points: 3
-        },
-        {
-            name: 'average',
-            points: 5
-        },
-        {
-            name: 'good',
-            points: 8
-        },
-        {
-            name: 'very-good',
-            points: 9
-        },
-        {
-            name: 'excelent',
-            points: 9.5
-        }
-    ]
-
-    const status = rateScale
-        .filter(p => {
-            return item >= p.points
-        })
-        .slice(-1)[0]
-    console.log(status)
     return (
-        // <div className="rating">
-        <div className={`rating rating--${status.name}`}>
-            <BsStarFill/>
+        <div className={'rating'}>
+            <BsStarFill className="rating__star"/>
             <span className='rating__points'>
-                {item}
+                {item.toFixed(1)}
             </span>
         </div>
-        // </div>
     )
 }
 
