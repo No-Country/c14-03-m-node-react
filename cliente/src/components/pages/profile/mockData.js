09cabf21bf636efb39d321289f3ac3d447b711fa
Caperactus <102335770+CaperAsDev@users.noreply.github.com>
2023-10-24 08:09:14 -0500

    Profile page done, some changes in styles and components, next: fix navbar routes: add profile

---
+++ cliente/src/components/pages/profile/mockData.js
@@ -0,0 +1,126 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+/* eslint-disable indent */
+/* eslint-disable semi */
+export const userData = {
+  userName: "Frutakus",
+  userImage:
+    "https://imgmedia.libero.pe/652x359/libero/original/2023/07/11/64adb0d5c47e4a7ef2214ebc.webp",
+  biography:
+    "Me gustan las series de comedia, llorar bajo la lluvia y escuchar a los pajaros cantar en la pradera como blanca nieves.",
+  status: [
+    {
+      name: "Viendo",
+      items: 5,
+      background_img:
+        "https://cdn.myanimelist.net/images/anime/1804/126632.jpg",
+    },
+    {
+      name: "Completado",
+      items: 20,
+      background_img:
+        "https://cdn.myanimelist.net/images/anime/1488/122008.jpg",
+    },
+    {
+      name: "Para ver",
+      items: 20,
+      background_img:
+        "https://cdn.myanimelist.net/images/anime/1015/138006.jpg",
+    },
+  ],
+  lists: [
+    {
+      name: "Comedia",
+      items: [
+        { name: "Konosuba", id: 1 },
+        { name: "Saiki", id: 2 },
+        { name: "Asobi Asobase", id: 3 },
+        { name: "Gintama", id: 5 },
+        { name: "Nichijou", id: 4 },
+      ],
+      id: 1,
+    },
+    {
+      name: "Para llorar",
+      items: [
+        { name: "Violet Evergarden", id: 7 },
+        { name: "FullMetal Alchemist", id: 8 },
+        { name: "Shigatsu wa kimi no uso", id: 10 },
+      ],
+      id: 2,
+    },
+    {
+      name: "Para sufrir",
+      items: [
+        { name: "Re: Zero", id: 11 },
+        { name: "Angel Beats", id: 12 },
+        { name: "Code GEass", id: 13 },
+        { name: "Made in abyss", id: 14 },
+      ],
+      id: 3,
+    },
+    {
+      name: "Pinches romances",
+      items: [
+        { name: "Love is War", id: 15 },
+        { name: "Kimi no na wa", id: 16 },
+        { name: "Gambare Douki-chan", id: 17 },
+        { name: "Kaichou wa maid-sama", id: 18 },
+        { name: "Nisekoi", id: 19 },
+        { name: "Tonari no Kaibutsu-kun", id: 20 },
+        { name: "Kimi ni todoke", id: 21 },
+      ],
+      id: 4,
+    },
+  ],
+  reviews: [
+    {
+      anime: {
+        name: "Gekkan Shoujo Nozaki-kun",
+        poster_img: "https://cdn.myanimelist.net/images/anime/5/66083.jpg",
+        score: 7.5,
+        id: 1
+      },
+      review: {
+        title: 'Recommended',
+        text: "If you're looking for an anime with romance and relationships, this isn't what you're looking for. If you're looking for an anime with endless hilarity and gags that runs off love and romance, this is what you're looking for. This anime thrives off misjudgments and the characters.",
+        date: '2015-02-10',
+        userScore: 8.2,
+        reviewLikes: 1,
+      },
+      id: 1,
+    },
+    {
+      anime: {
+        name: "Sono Bisque Doll wa Koi wo Suru",
+        poster_img: "https://cdn.myanimelist.net/images/anime/1179/119897.jpg",
+        score: 8.5,
+        id: 2
+      },
+      review: {
+        title: 'Not Recommended',
+        text: "I cannot emphasize this enough, but My Dress-Up Darling is an ECCHI rom-com. Forget the synopsis about the interesting life of a cosplayer and doll-maker respectively, this is a terrible high school slice of life with some of the worst ecchi sequences I have ever seen. I would like to preface at the beginning of this review that My Dress-Up Darling has become one of my most hated anime. Admittedly, that doesn’t excuse me from just trashing the show entirely, so I will still try to be as objective as I can, since this review is meant for potential viewers.",
+        date: '2022-03-26',
+        userScore: 8.2,
+        reviewLikes: 10,
+      },
+      id: 2,
+    },
+    {
+      anime: {
+        name: "Sakura-sou no Pet na Kanojo",
+        poster_img: "https://cdn.myanimelist.net/images/anime/4/43643.jpg",
+        score: 9.2,
+        id: 3
+      },
+      review: {
+        title: 'Mixed feelings',
+        text: "The very first line in the anime coming from the protagonist himself. Frankly, I felt exactly the same after watching the first dozen episodes of Sakurasou no Pet na Kanojo. J.C. Staff, a studio which animated numerous titles such as Toradora, Zero no Tsukaima, Toaru Majutsu no Index, now it's Sakurasou no Pet na Kanojo or in English, 'The Pet Girl of Sakurasou'.",
+        date: '2013-04-30',
+        userScore: 8.7,
+        reviewLikes: 15,
+      },
+      id: 3,
+    },
+  ],
+};
--- cliente/src/context/main.jsx
+++ cliente/src/context/main.jsx
@@ -1,43 +1,16 @@
-import React, { createContext, useMemo } from 'react'
+import React, { createContext, useEffect, useMemo, useState } from 'react'
 
 export const GenericContext = createContext({})
 
 // eslint-disable-next-line react/prop-types
 export function GenericProvider ({ children }) {
-//   const [cartProducts, setCartProducts] = useState([] as Product[]);
-//   const [myOrders, setMyOrders] = useState([] as Order[]);
-//   const [isDetailOpen, setIsDetailOpen] = useState(false);
-//   const [isMyOrderOpen, setIsMyOrderOpen] = useState(false);
-//   const [productSelected, setProductSelected] = useState({} as Product);
-//   const [products, setProducts] = useState([] as Product[]);
-//   const [productsToRender, setProductsToRender] = useState([] as Product[]);
-//   const [searchedValue, setSearchedValue] = useState(' ');
+    //   const [cartProducts, setCartProducts] = useState([] as Product[]);
 
     const values = useMemo(() => ({
         // cartProducts,
-        // setCartProducts,
-        // isDetailOpen,
-        // setIsDetailOpen,
-        // productSelected,
-        // setProductSelected,
-        // isMyOrderOpen,
-        // setIsMyOrderOpen,
-        // myOrders,
-        // setMyOrders,
-        // products,
-        // setProducts,
-        // searchedValue,
-        // setSearchedValue,
-        // productsToRender,
-        // setProductsToRender
     }), [
         // cartProducts,
         // isDetailOpen,
-        // productSelected,
-        // isMyOrderOpen,
-        // products,
-        // productsToRender,
-        // searchedValue
     ])
     return (
         <GenericContext.Provider value={values}>
